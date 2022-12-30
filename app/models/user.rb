204dd46b7bdce42f3a3614f384cfefd64a187411
shindo244244 <t.shindo244@gmail.com>
2022-12-30 20:10:39 +0900

    Rubocop実施、コメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,} do
-   validates :family_name
-   validates :first_name
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :family_name
+    validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
     validates :family_name_kana
     validates :first_name_kana
-   end
-        
+  end
+
   validates :nickname, presence: true
   validates :birthday, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
     gimei = Gimei.name
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 8, mix_case: false)}
-    password_confirmation {password}
-    family_name           {gimei.last.kanji}
-    first_name            {gimei.first.kanji}
-    family_name_kana      {gimei.last.katakana}
-    first_name_kana       {gimei.first.katakana}
-    birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 8, mix_case: false) }
+    password_confirmation { password }
+    family_name           { gimei.last.kanji }
+    first_name            { gimei.first.kanji }
+    family_name_kana      { gimei.last.katakana }
+    first_name_kana       { gimei.first.katakana }
+    birthday              { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
