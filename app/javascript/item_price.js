c60e69b70f36a0402dfc5e2ef76675b24a8ee782
shindo244244 <t.shindo244@gmail.com>
2023-01-09 20:30:04 +0900

    商品出品機能　修正1

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,9 +3,9 @@ window.addEventListener('load', () => {
 
    priceInput.addEventListener("input", () => {
      const TaxDom = document.getElementById("add-tax-price");
-     TaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+     TaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
      const ProfitDom = document.getElementById("profit");
     
-     ProfitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+     ProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,5 +14,5 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1 } 
   validates :prefecture_id, numericality: { other_than: 1 } 
   validates :delivery_time_id, numericality: { other_than: 1 } 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true}
 end
