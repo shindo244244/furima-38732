840309fb60b43f27c340aadc96117b480d3b8c5f
shindo244244 <t.shindo244@gmail.com>
2023-01-10 13:34:55 +0900

    商品一覧表示機能

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2023_01_05_081855) do
     t.integer "delivery_time_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
