b09c034f6b9bcae28d04a6b3257c56bc0d48c254
shindo244244 <t.shindo244@gmail.com>
2023-01-11 17:45:19 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,17 +19,36 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-
+    return if @item.user.id == current_user.id
+    redirect_to action: :index
   end
 
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.user.id == current_user.id
+    @item.destroy
+    end
+    redirect_to root_path
+  end
+  
   private
 
   def item_params
     params.require(:item).permit(:item_name, :explain, :price, :category_id, :state_id, :delivery_charge_id, :prefecture_id,
                                  :delivery_time_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
